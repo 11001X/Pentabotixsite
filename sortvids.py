bb3faa6ec21e66907eda2862a4816759d082e867
nihalcshah <nihalcshah@gmail.com>
2023-03-02 16:52:43 -0500

    videos organization

--- sortvids.py
+++ sortvids.py
@@ -9,6 +9,6 @@ for i in os.listdir("./vids/"):
     file_path = "./vids/"+i  # change to your own video path
     vid = cv2.VideoCapture(file_path)
     res = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)/vid.get(cv2.CAP_PROP_FRAME_WIDTH)
-    if res>1:
-        vidsfile.write(templates.replace("__",i)+"\n")
+    # if res>1:
+    vidsfile.write(templates.replace("__",i)+"\n")
 
