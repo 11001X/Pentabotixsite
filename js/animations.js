6b9768bf9a288c5671031600e76c1acc008b4efd
nihalcshah <nihalcshah@gmail.com>
2023-04-16 23:35:56 -0400

    revamp look

--- js/animations.js
+++ js/animations.js
@@ -1,17 +1,12 @@
-const observer = new IntersectionObserver(entries => {
-    console.log('S')
-    // Loop over the entries
-    entries.forEach(entry => {
-      // If the element is visible
-      if (entry.isIntersecting) {
-        // Add the animation class
-        entry.target.classList.add('squareanimate');
-      }
-    });
+const images = document.querySelectorAll('.loading-image');
+
+images.forEach(image => {
+  image.addEventListener('load', () => {
+    const progressBar = image.parentNode.querySelector('.progress');
+    progressBar.style.width = '100%';
   });
 
-// let courses = document.getElementById("Courses")
-let cards = document.getElementsByClassName('col-lg-3')
-Array.from(cards).forEach( e=>{
-    observer.observe(e);
-})
+  image.addEventListener('error', () => {
+    console.error(`Error loading image: ${image.src}`);
+  });
+});
